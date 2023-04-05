b06e1bd9ee1ca7995feb294e4776b35a6d2c00f7
reyesenamorado97 <97928078+reyesenamorado97@users.noreply.github.com>
2023-04-05 12:36:58 -0400

    Moved images and src to root directory

---
+++ src/login/login.js
@@ -0,0 +1,10 @@
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+function UpdateEmail()
+
+function CheckEmail() {
+    
+    console.log(email)
+}
+console.log(email)
