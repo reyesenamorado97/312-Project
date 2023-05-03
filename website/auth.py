17a407799d172a9591b02b5a5127cccc263e513c
reyesenamorado97 <reyesenamorado@googlemail.com>
2023-05-03 14:32:55 -0400

    Added the login page. Logo button in the navbar now redirects to /welcome path.

--- website/auth.py
+++ website/auth.py
@@ -6,3 +6,7 @@ auth =Blueprint('auth',__name__)
 @auth.route("/signupForm")
 def signupForm():
     return render_template("signup.html")
+
+@auth.route("/loginForm")
+def loginForm():
+    return render_template("login.html")
