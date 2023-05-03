025072f536d458304ed2f7658a9984ef6f75bee3
Albi Kozeli <albikoze@buffalo.edu>
2023-05-03 13:19:13 -0400

    skeleton

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY']='hsfjdsfjnoaolnsuveueoop'
 
+    from .auth import auth
+    from .game import game
+    from .lobby import lobby
+    from .user_edit import user_edit
+
+    app.register_blueprint(auth,url_prefix="/")
+    app.register_blueprint(game,url_prefix="/game")
+    app.register_blueprint(lobby,url_prefix="/lobby")
+    app.register_blueprint(user_edit,url_prefix="/user_edit")
+
     return app
