f1c2596dcd0bc1dd8ec07f26b6ccfa2efbc503d4
reyesenamorado97 <reyesenamorado@googlemail.com>
2023-05-03 21:59:05 -0400

    Basic formatting for Home page is done

--- website/lobby.py
+++ website/lobby.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template, send_from_directory, redirect
 
 lobby =Blueprint('lobby',__name__)
+
+@lobby.route("/home")
+def loadHome():
+    return render_template("home.html")
