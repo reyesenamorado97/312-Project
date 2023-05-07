062ace7d1588ca08c533737d28307b13effe582f
reyesenamorado97 <97928078+reyesenamorado97@users.noreply.github.com>
2023-05-07 13:10:25 -0400

    Delete website directory

--- website/game.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, render_template, send_from_directory, redirect
-
-#--- Importing the database object from __init__ (website project)
-from website import database
-
-game =Blueprint('game',__name__)
