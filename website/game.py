380075ee18980587845711254d45c99f8800dbc8
reyesenamorado97 <reyesenamorado@googlemail.com>
2023-05-07 13:21:01 -0400

    last changes before overhaul

--- website/game.py
+++ website/game.py
@@ -1,3 +1,11 @@
 from flask import Blueprint, render_template, send_from_directory, redirect
 
 game =Blueprint('game',__name__)
+
+@game.route("/lobby")
+def loadLobby():
+    return render_template("lobby.html")
+
+@game.route("/playing")
+def playingGame():
+    return render_template("game.html")
