7d4c76c38edac0307badf498532b8e28fa3ef3b0
reyesenamorado97 <97928078+reyesenamorado97@users.noreply.github.com>
2023-05-12 13:07:39 -0400

    Delete src directory

--- src/index/index.js
+++
@@ -1,8 +0,0 @@
-// Burger Dropdown Functionality
-const burgerIcon = document.querySelector("#burger");
-const navbarMenu = document.querySelector("#nav-links");
-
-// If we click of the burger, toggle the active state on/off
-burgerIcon.addEventListener("click", () => {
-	navbarMenu.classList.toggle("is-active");
-});
